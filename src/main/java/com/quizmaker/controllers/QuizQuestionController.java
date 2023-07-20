59c13811049f79c272c47732a5325e810b08b964
Matteo <matteodibs@gmail.com>
2023-07-20 17:02:35 +0200

    fix

--- src/main/java/com/quizmaker/controllers/QuizQuestionController.java
+++ src/main/java/com/quizmaker/controllers/QuizQuestionController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "quizzes")
+@RequestMapping(path = "quiz-question")
 public class QuizQuestionController {
 }
